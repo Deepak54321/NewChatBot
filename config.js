997e6aa60c6bd32ddb1e25707e09038bde736754
Deepak54321 <31242754+Deepak54321@users.noreply.github.com>
2017-11-09 22:02:40 -0800

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
     FB_PAGE_TOKEN: 'EAAVUvjjH8zsBAHvskgVKOT46dIYjbX9O7wZB5hL13idExhD0pZCZAnlleHWuXuZB9sAiVsH6z6Xg5Ulf6dZCq5zlaSVqesrUeEgaHPTeasZCHtiPdQyHrZBrXM1aB6jS47ZAzaZA7hSnL7hYkB0T6fq6t8raXfah3fwiJvcLGDEpWzwZDZD',
     FB_VERIFY_TOKEN: 'abcd12345',
-    API_AI_CLIENT_ACCESS_TOKEN: '62f07ddbd7604016af6554aad998abdc',
+    API_AI_CLIENT_ACCESS_TOKEN: 'a8f9b2a9bb9540bc85c6129d94e86f26',
     FB_APP_ID: '1500550856700731',
     FB_APP_SECRET: 'edca2fc9178aaf22c2f8aae055cadc2c',
-    SERVER_URL: "https://nodejsapp12345.herokuapp.com",
+    SERVER_URL: "https://dychatbot.herokuapp.com",
 };
