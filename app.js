f08a0338e55cc73cd4252075b7c979e59095002f
Deepak54321 <dmishra@yamaha-motor-india.com>
2017-11-14 21:49:24 -0800

    insert in db10

--- app.js
+++ app.js
@@ -1029,7 +1029,7 @@ function greetUserText(userId) {
                         return console.error('Error acquiring client', err.stack);
                     }
                     var rows = [];
-                    client.query("SELECT id FROM users WHERE fb_id='${userId}' LIMIT 1",
+                    client.query("SELECT id FROM user WHERE fb_id='${userId}' LIMIT 1",
                         function(err, result) {
                             console.log('query result ' + result);
                             if (err) {
