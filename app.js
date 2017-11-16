b876cf80bacee88e2fb734dfdae4f1a70e2f0204
Deepak54321 <dmishra@yamaha-motor-india.com>
2017-11-15 21:04:34 -0800

    insert in db30

--- app.js
+++ app.js
@@ -353,7 +353,7 @@ function handleApiAiAction(sender, action, responseText, contexts, parameters) {
                     sendQuickReply(sender,emailContent,comrply);
                     //responseText=emailContent;
                 }
-              /*  var connectionString = "postgres://hplemmqnodrktw:46fecc18d4edb226ae70341dddb67303f980b4992be13d1512b967e9d1c26656@ec2-54-243-252-232.compute-1.amazonaws.com:5432/d1d9dpk0dupij6";
+                var connectionString = "postgres://hplemmqnodrktw:46fecc18d4edb226ae70341dddb67303f980b4992be13d1512b967e9d1c26656@ec2-54-243-252-232.compute-1.amazonaws.com:5432/d1d9dpk0dupij6";
                 var pgClient = new pg.Client(connectionString);
                 pgClient.connect();
                 var rows = [];
@@ -375,7 +375,7 @@ function handleApiAiAction(sender, action, responseText, contexts, parameters) {
                             }
                         }
                     });
-                pgClient.end();*/
+                //pgClient.end();
 
                 sendTextMessage(sender, responseText);
             }
